370cdb3d77500dd837144393af45c27704899558
chrispower254 <chrispower254@gmail.com>
2022-03-20 10:59:05 +0100

    Added filtering in backend

--- backend/opm_algo/event_log_updater.py
+++ backend/opm_algo/event_log_updater.py
@@ -2,8 +2,14 @@ import pandas as pd
 import os
 from pm4py.objects.conversion.log import converter as log_converter
 from pm4py.util import constants
+from pm4py.algo.filtering.pandas.attributes import attributes_filter
+import json
+import pm4py
 
 def event_log_updater(type):
+    f = open('config.json')
+    config = json.load(f)
+
     if type == 'csv':
         #cwd = os.getcwd()  # Get the current working directory (cwd)
         #files = os.listdir(cwd)  # Get all the files in that directory
@@ -12,7 +18,17 @@ def event_log_updater(type):
         event_log = log_converter.apply(log_csv, parameters={constants.PARAMETER_CONSTANT_CASEID_KEY: "case:concept:name",
                                                              constants.PARAMETER_CONSTANT_ACTIVITY_KEY: "concept:name",
                                                              constants.PARAMETER_CONSTANT_TIMESTAMP_KEY: "time:timestamp"})
+
+        event_log_filtered = event_log
+        for attribute in config['filters']:
+            key_arr = config['filters'][attribute]
+            event_log_filtered = pm4py.filter_event_attribute_values(event_log_filtered, attribute, key_arr,level='event')
+
     if type == 'xes':
         event_log = 'lol nuffing'
+        event_log_filtered = event_log
+        for attribute in config['filters']:
+            key_arr = config['filters'][attribute]
+            event_log_filtered = pm4py.filter_event_attribute_values(event_log_filtered, attribute, key_arr,level='event')
 
-    return event_log
+    return event_log_filtered
