b8d278489bb9bed683696b5635a2f03b15d341ad
Daniel F <st148939@stud.uni-stuttgart.de>
2022-03-22 21:17:53 +0100

    Adidas Events

---
+++ backend/event_generation/sendAdidasEvents.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import json
+import time 
+import requests
+
+filepath = '/Users/daniel/Documents/GitHub.nosync/OPMthis/backend/files/adidas.csv'
+url = 'http://localhost:8082/topics/adidas'
+headers = {'content-type': 'application/vnd.kafka.json.v2+json'}
+
+def sendPost():
+    df = pd.read_csv(filepath, delimiter=';')
+    
+    for index, row in df.iterrows(): 
+        json_dict = {
+            "case": row[0],
+            "event": row[1],
+            "timestamp": row[2],
+            "device_type": row[3],
+            "country": row[4],
+            "continent": row[5],
+            "city": row[6],
+        }
+        json_raw = json.dumps(json_dict)
+        data = '{"records":[{"value":'+ json_raw + '}]}';
+        r = requests.post(url, data=data, headers=headers)
+        time.sleep(5)
+
--- backend/event_generation/set_filters.py
+++ backend/event_generation/set_filters.py
@@ -1,8 +1,9 @@
 import json
 
-
+config_file = 'config.json'
+#config_file = 'configAdidas.json'
 def set_filters(filter_json):
-    with open('config.json') as f:
+    with open(config_file) as f:
         config = json.load(f)
 
     #for filter in config['filters']:
@@ -10,7 +11,7 @@ def set_filters(filter_json):
 
     config['filters'] = filter_json
 
-    with open('config.json', 'w') as f:
+    with open(config_file, 'w') as f:
         config = json.dump(config, f)
 
     print("Set filters finished")
